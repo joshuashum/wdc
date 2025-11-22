659bfe69c80e483fec9d239acb2cef3f949821c5
Joshua Shum <joshua.ly.shum@gmail.com>
2025-11-22 12:43:08 -0500

    fixed spacing

--- 03_Interactivity_with_Javascript/02_Assignments/A1/js/gallery.js
+++ 03_Interactivity_with_Javascript/02_Assignments/A1/js/gallery.js
@@ -15,6 +15,7 @@ function upDate(previewPic) {
 function unDo() {
     document.getElementById("image").style.backgroundImage = "url('')";
     document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+    
     /* In this function you should 
        1) Update the url for the background image of the div with the id = "image" 
        back to the orginal-image.  You can use the css code to see what that original URL was
